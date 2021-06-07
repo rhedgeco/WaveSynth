f990bf45f897b24e19eff428bce9bdf3c17ced2f
rhedgeco <rhedgeco@gmail.com>
2021-06-07 10:11:39 -0700

    Added phasing to function generator

--- Assets/Scripts/WaveSynth/Generators/FunctionGenerator.cs
+++ Assets/Scripts/WaveSynth/Generators/FunctionGenerator.cs
@@ -2,19 +2,28 @@
 using System.Linq;
 using UnityEngine;
 using WaveSynth.Triggers;
+using Random = System.Random;
 
 namespace WaveSynth.Generators
 {
     public abstract class FunctionGenerator : AudioOutput
     {
+        // @formatter:off
+        [Header("Generator")]
+        [SerializeField] [Range(0, 1)] private float amplitude = 0.3f;
+        [SerializeField] [Range(0, 1)] private float phase = 0;
+        [SerializeField] [Range(0, 1)] private float randomPhase = 1;
+        [SerializeField] private AudioTrigger trigger;
+        
+        [Header("Envelope")]
         [SerializeField] private float attackMS = 2f;
         [SerializeField] private float releaseMS = 15f;
-        [SerializeField] [Range(0, 1)] private float amplitude = 0.3f;
-        [SerializeField] private AudioTrigger trigger;
+        // @formatter:on
 
         private float _attackSpeed = 1;
         private float _releaseSpeed = 1;
         private List<EnvelopeData> _envelopes = new List<EnvelopeData>();
+        private Random _random = new Random();
 
         protected override void ProcessBuffer(ref float[] buffer)
         {
@@ -22,20 +31,20 @@ namespace WaveSynth.Generators
             _attackSpeed = (GlobalAudioController.SampleRate / 1000) * attackMS;
             _releaseSpeed = (GlobalAudioController.SampleRate / 1000) * releaseMS;
             List<TriggerFrequency> newFrequencies = trigger.CalculateActiveFrequencies(buffer.Length);
-            
-            // prune and correct envelope map
+
+            // prune envelope map
             for (int i = _envelopes.Count - 1; i >= 0; i--)
             {
                 EnvelopeData data = _envelopes[i];
                 data.Active = newFrequencies.Contains(data.Trigger);
                 if (!data.Active && data.Amplitude <= 0.0001f) _envelopes.RemoveAt(i);
             }
-            
+
             // add new frequencies to envelope map
             foreach (TriggerFrequency frequency in newFrequencies)
             {
                 if (_envelopes.Any(data => data.Trigger == frequency)) continue;
-                _envelopes.Add(new EnvelopeData(frequency));
+                _envelopes.Add(new EnvelopeData(frequency, (float) _random.NextDouble() - 0.5f));
             }
 
             for (int i = 0; i < buffer.Length; i++)
@@ -53,16 +62,20 @@ namespace WaveSynth.Generators
             {
                 EnvelopeData envelope = _envelopes[i];
                 float freqValue = envelope.Trigger.Frequency;
-                float loop = envelope.BufferProgress % (GlobalAudioController.SampleRate / freqValue);
-                float wavePart = WaveFunction(loop * freqValue / GlobalAudioController.SampleRate);
-
-                // apply attack
-                if (envelope.Active && envelope.Amplitude < 1) envelope.ModifyAmplitude(1 / _attackSpeed);
                 
-                // apply release
-                if (!envelope.Active && envelope.Amplitude > 0) envelope.ModifyAmplitude(-1 / _releaseSpeed);
-
+                // apply phasing to progress
+                float progress = envelope.BufferProgress % (GlobalAudioController.SampleRate / freqValue);
+                progress *= freqValue / GlobalAudioController.SampleRate;
+                progress += phase;
+                progress += envelope.RandomPhase * randomPhase;
+                progress %= 1;
+                
+                float wavePart = WaveFunction(progress);
                 sample += wavePart * envelope.Amplitude * amplitude;
+
+                // apply attack and release
+                if (envelope.Active && envelope.Amplitude < 1) envelope.ModifyAmplitude(1 / _attackSpeed);
+                if (!envelope.Active && envelope.Amplitude > 0) envelope.ModifyAmplitude(-1 / _releaseSpeed);
                 envelope.AddToBuffer(1);
             }
 
@@ -76,13 +89,15 @@ namespace WaveSynth.Generators
             public TriggerFrequency Trigger { get; }
             public int BufferProgress { get; private set; }
             public float Amplitude { get; private set; }
+            public float RandomPhase { get; }
             public bool Active { get; set; }
 
-            public EnvelopeData(TriggerFrequency trigger)
+            public EnvelopeData(TriggerFrequency trigger, float randomPhase)
             {
                 Trigger = trigger;
                 BufferProgress = 0;
                 Amplitude = 0;
+                RandomPhase = randomPhase;
                 Active = true;
             }
 
