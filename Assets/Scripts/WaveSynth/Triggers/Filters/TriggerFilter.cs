0264e84dab5c0a5349a3a68593705614a08a436b
rhedgeco <rhedgeco@gmail.com>
2021-06-07 10:12:47 -0700

    Added basic structure for trigger filters

---
+++ Assets/Scripts/WaveSynth/Triggers/Filters/TriggerFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace WaveSynth.Triggers.Filters
+{
+    public abstract class TriggerFilter : AudioTrigger
+    {
+        [SerializeField] private AudioTrigger triggerInput;
+        
+        protected override List<TriggerFrequency> ProcessFrequencies(int bufferLength)
+        {
+            return ProcessTriggerFilter(triggerInput.CalculateActiveFrequencies(bufferLength));
+        }
+
+        protected abstract List<TriggerFrequency> ProcessTriggerFilter(List<TriggerFrequency> frequencies);
+    }
+}
