db613d2a6d5b1fe515d49d8d939251c23c460a5b
rhedgeco <rhedgeco@gmail.com>
2021-06-10 13:27:57 -0700

    fixed channel splitting of function generator

--- Assets/Scripts/WaveSynth/WaveOutputs/WaveGenerators/FunctionGenerator.cs
+++ Assets/Scripts/WaveSynth/WaveOutputs/WaveGenerators/FunctionGenerator.cs
@@ -8,24 +8,26 @@ namespace WaveSynth.WaveOutputs.WaveGenerators
     {
         [SerializeField] private WaveTrigger trigger;
         [SerializeField] [Range(0, 1)] private float amplitude = 0.8f;
-        
+
         private Dictionary<WaveTrigger.Trigger, int> _bufferProgress =
             new Dictionary<WaveTrigger.Trigger, int>();
 
         protected override void ProcessWaveBuffer(ref float[] buffer)
         {
-            for (int i = 0; i < buffer.Length; i++)
+            for (int i = 0; i < buffer.Length; i += 2)
             {
-                buffer[i] = 0;
+                buffer[i + 0] = buffer[i + 1] = 0;
                 HashSet<WaveTrigger.Trigger> triggers = trigger.GetTriggers(1);
                 foreach (WaveTrigger.Trigger frequencyTrigger in triggers)
                 {
-                    if (!_bufferProgress.ContainsKey(frequencyTrigger)) 
+                    if (!_bufferProgress.ContainsKey(frequencyTrigger))
                         _bufferProgress.Add(frequencyTrigger, 0);
                     int progress = _bufferProgress[frequencyTrigger]++;
                     float phase = progress % (WaveSettings.SampleRate / frequencyTrigger.Frequency);
                     phase *= frequencyTrigger.Frequency / WaveSettings.SampleRate;
-                    buffer[i] += SampleFunction(phase % 1) * amplitude * frequencyTrigger.Amplitude;
+                    float sample = SampleFunction(phase % 1) * amplitude * frequencyTrigger.Amplitude;
+                    buffer[i + 0] += sample;
+                    buffer[i + 1] += sample;
                 }
             }
         }
